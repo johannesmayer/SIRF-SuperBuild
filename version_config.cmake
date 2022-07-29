83ecb7242c497f5abb0191a43558a1d024dddd8b
johannesmayer <johannes.mayer@ptb.de>
2022-07-29 16:35:55 +0200

    Switched to different branch to build against

--- version_config.cmake
+++ version_config.cmake
@@ -197,7 +197,7 @@ if (DEVEL_BUILD)
   set(DEFAULT_CCPi-Regularisation-Toolkit_TAG origin/master)
 
 else()
-  set(DEFAULT_SIRF_TAG v3.2.0)
+  set(DEFAULT_SIRF_TAG origin/qfix-dcm-attributes)
 
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
