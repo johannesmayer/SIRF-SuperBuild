9e6ea0aa036dcec80b725220d791688e97514079
johannesmayer <johannes.mayer1991@gmail.com>
2023-03-15 17:32:24 +0100

    Working version for sirf container using sirf to write dicoms

--- docker/xnat/execution.sh
+++ docker/xnat/execution.sh
@@ -4,4 +4,6 @@ set +euo pipefail
 conda activate base
 set -euo pipefail
 
+exec gadgetron &
 exec python /recon/mr_cine_recon.py /input /output
+exec kill $(pidof gadgetron)
--- docker/xnat/recon/mr_cine_recon.py
+++ docker/xnat/recon/mr_cine_recon.py
@@ -1,12 +1,74 @@
+import os
+import shutil
+
+from pathlib import Path
+import sirf_preprocessing as preprocess
+import sirf.Gadgetron as pMR
+
 import sys
 from pathlib import Path
-from sirf_recon import main_cine_recon as main
 
 ### looped reconstruction over files in input path
 path_in  = Path(sys.argv[1])
 path_out = Path(sys.argv[2])
 
-if __name__ == __main__:
-    success = main(path_in, path_out)
+
+def main_cine_recon(path_in, fpath_output_prefix):
+
+    print(f"Reading from {path_in}, writing into {fpath_output_prefix}")
+    assert os.access(path_in, os.R_OK), f"You don't have write permission in {path_in}"
+    assert os.access(fpath_output_prefix, os.W_OK), f"You don't have write permission in {fpath_output_prefix}"
+
+    list_rawdata = sorted(path_in.glob("*.h5"))
+    success = True
+
+    for fname_raw in list_rawdata:
+        mr_data = preprocess.equally_fill_cardiac_phases(str(fname_raw))
+        success *= sirf_cine_recon(mr_data, fpath_output_prefix)
+
+    print(f'Reconstruction finished successful? {success}')
+
+    return int(not success)
+
+def sirf_cine_recon(mr_rawdata: pMR.AcquisitionData, fprefix_out: Path):
+
+    # Pre-process this input data.
+    # (Currently this is a Python script that just sets up a 3 chain gadget.
+    # In the future it will be independent of the MR recon engine.)
+    preprocessed_data = pMR.preprocess_acquisition_data(mr_rawdata)
+
+    # Perform reconstruction of the preprocessed data.
+    # 1. set the reconstruction to be for Cartesian GRAPPA data.
+
+    recon_gadgets = ['AcquisitionAccumulateTriggerGadget',
+            'B2B:BucketToBufferGadget',
+            'GenericReconCartesianReferencePrepGadget',
+            'GRAPPA:GenericReconCartesianGrappaGadget',
+            'GenericReconFieldOfViewAdjustmentGadget',
+            'GenericReconImageArrayScalingGadget',
+            'ImageArraySplitGadget',
+            # 'PhysioInterpolationGadget(phases=30, mode=0, first_beat_on_trigger=false, interp_method=BSpline)',
+            ]
+    recon = pMR.Reconstructor(recon_gadgets)
+
+    # 2. set the reconstruction input to be the data we just preprocessed.
+    recon.set_input(preprocessed_data)
+
+    # 3. run (i.e. 'process') the reconstruction.
+    print('---\n reconstructing...\n')
+    recon.process()
+
+    # retrieve reconstruced image and G-factor data
+    # image_data = recon.get_output('Image PhysioInterp')
+    image_data = recon.get_output('image')
+    print(f"We have {image_data.number()} images to write.")
+    image_data = image_data.abs()
+    image_data.write(str(fprefix_out / "sirfrecon.dcm"))
+
+    return True
+
+
+if __name__ == "__main__":
+    success = main_cine_recon(path_in, path_out)
     sys.exit(success)
 
