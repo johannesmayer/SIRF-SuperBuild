0cceb6f2fcee4c758588a92b6e40dd7801ec98f9
johannesmayer <johannes.mayer1991@gmail.com>
2023-03-16 19:11:25 +0100

    Modified Dockerfile s.t. entrypoint runs as root

--- docker/xnat/execution.sh
+++ docker/xnat/execution.sh
@@ -1,9 +1,5 @@
 #!/bin/bash --login
 
-set +euo pipefail
-conda activate base
-set -euo pipefail
-
 exec gadgetron &
 exec python /recon/mr_cine_recon.py /input /output
 exec kill $(pidof gadgetron)
